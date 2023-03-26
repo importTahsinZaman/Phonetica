737075c30ed7cceef07a0d6b724e6d601d0391c5
importTahsinZaman <tahsinz21366@gmail.com>
2023-03-25 21:51:33 -0400

    Added word definitions

--- app/screens/TranslationScreen.tsx
+++ app/screens/TranslationScreen.tsx
@@ -2,21 +2,30 @@ import { useState, useEffect } from "react";
 
 import { DEEPL_KEY, OPEN_AI_API_KEY } from "@env";
 
-import { SafeAreaView, Text, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Alert,
+} from "react-native";
 import { Configuration, OpenAIApi } from "openai";
 
 const TranslationScreen = ({ route, navigation }) => {
-  const [finalizedText, setFinalizedText] = useState("");
-  const [translatedText, setTranslatedText] = useState("");
-
   const { sentence } = route.params;
+  const [finalizedText, setFinalizedText] = useState("");
+  const [wordsList, setWordsList] = useState([]);
+  const [translatedText, setTranslatedText] = useState("");
+  const [definitionModalVisible, setDefinitionModalVisible] = useState(false);
+  const [definitionExplanation, setDefinitionExplanation] = useState("");
+
+  const configuration = new Configuration({
+    apiKey: OPEN_AI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
   const fixText = async () => {
     console.log("Running fix text");
-    const configuration = new Configuration({
-      apiKey: OPEN_AI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
 
     await openai
       .createCompletion({
@@ -31,7 +40,7 @@ const TranslationScreen = ({ route, navigation }) => {
       .then((result) => {
         let response1 = JSON.parse(result.request._response);
         setFinalizedText(response1.choices[0].text.trimStart());
-        console.log(response1.choices[0].text.trimStart());
+        setWordsList(response1.choices[0].text.trimStart().match(/\b(\w+)\b/g));
       });
   };
 
@@ -61,6 +70,23 @@ const TranslationScreen = ({ route, navigation }) => {
       .catch((error) => console.log("error", error));
   };
 
+  const generateDefinitionExplanation = async (word: string) => {
+    await openai
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: `Explain the definition and usage of the word "${word}" in this context to someone who only speaks Spanish. Keep the word in English and within quotation marks whenever referring to it.`,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      .then((result) => {
+        let response1 = JSON.parse(result.request._response);
+        setDefinitionExplanation(response1.choices[0].text.trimStart());
+      });
+  };
+
   useEffect(() => {
     console.log("Running useEffect for fixing Text");
     fixText();
@@ -68,15 +94,64 @@ const TranslationScreen = ({ route, navigation }) => {
 
   return (
     <SafeAreaView>
+      <Text>{finalizedText}</Text>
+      <Text></Text>
+      <Text>{translatedText}</Text>
+      <Text></Text>
+
       <TouchableOpacity
         onPress={() => {
           translateText(finalizedText);
         }}
+        className="items-center"
       >
-        <Text>{finalizedText}</Text>
+        <Text className="text-xl font-bold text-black">Translate</Text>
       </TouchableOpacity>
-      <Text></Text>
-      <Text>{translatedText}</Text>
+
+      <TouchableOpacity
+        onPress={() => setDefinitionModalVisible(true)}
+        className="items-center"
+      >
+        <Text className="text-xl font-bold text-black">Definition</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => console.log("")}
+        className="items-center"
+      >
+        <Text className="text-xl font-bold text-black">Pronunciation</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={definitionModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setDefinitionModalVisible(!definitionModalVisible);
+        }}
+      >
+        <SafeAreaView>
+          <TouchableOpacity
+            className="p-10 bg-[#2196F3]"
+            onPress={() => setDefinitionModalVisible(!definitionModalVisible)}
+          >
+            <Text>Hide Modal</Text>
+          </TouchableOpacity>
+          <Text>{definitionExplanation}</Text>
+          {wordsList.map((word) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  generateDefinitionExplanation(word);
+                }}
+              >
+                <Text>{word}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </SafeAreaView>
+      </Modal>
     </SafeAreaView>
   );
 };
