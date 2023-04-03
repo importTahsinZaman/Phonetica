bdb622274d4e177ebc5af0add6845c4c1b21e5db
importTahsinZaman <tahsinz21366@gmail.com>
2023-04-03 02:10:27 -0400

    Fixed Font Error

--- app/components/CustomText.tsx
+++ app/components/CustomText.tsx
@@ -1,4 +1,6 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { Text } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import {
   useFonts,
   SpaceGrotesk_300Light,
@@ -9,6 +11,8 @@ import {
 } from "@expo-google-fonts/space-grotesk";
 
 const CustomText = ({ style = {}, ...props }) => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   let [fontsLoaded] = useFonts({
     SpaceGrotesk_300Light,
     SpaceGrotesk_400Regular,
@@ -16,6 +20,14 @@ const CustomText = ({ style = {}, ...props }) => {
     SpaceGrotesk_600SemiBold,
     SpaceGrotesk_700Bold,
   });
+
+  if (!fontsLoaded) {
+    SplashScreen.preventAutoHideAsync();
+    return null;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   const textStyles = {
     fontFamily: "SpaceGrotesk_400Regular",
   };
