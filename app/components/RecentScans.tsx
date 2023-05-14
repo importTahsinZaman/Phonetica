1b4210ceca9af76c17f7cf2696753a668425b7c1
importTahsinZaman <tahsinz21366@gmail.com>
2023-05-13 20:11:44 -0400

    Added basic styles for recent scans carousel

--- app/components/RecentScans.tsx
+++ app/components/RecentScans.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Dimensions, Text, View } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
+import CustomText from "./CustomText";
 
 const RecentScans = ({}) => {
   const PAGE_WIDTH = Dimensions.get("window").width;
@@ -17,17 +18,39 @@ const RecentScans = ({}) => {
         pagingEnabled={false}
         snapEnabled={false}
         style={{ width: PAGE_WIDTH }}
-        data={[...new Array(12).keys()]}
+        data={[...new Array(5).keys()]}
         renderItem={({ index }) => (
-          <View
-            style={{
-              flex: 1,
-              borderWidth: 1,
-              justifyContent: "center",
-            }}
-            className="m-2 rounded-xl bg-red-100 shadow-sm"
-          >
-            <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
+          <View className="m-2 flex-1 rounded-xl bg-white shadow justify-center flex">
+            <View className="p-2.5">
+              <CustomText>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deleniti animi similique facere adipisci architecto ipsam cum.
+                Quae cum laboriosam quis eos pariatur doloribus tempore animi
+                nesciunt totam, ea vero assumenda saepe quisquam inventore
+                impedit, praesentium optio obcaecati ducimus possimus labore
+                doloremque sunt. Minus laborum ratione distinctio, a quam quod
+                ut officia repellat officiis veritatis laudantium dicta aliquam
+                consequuntur minima perspiciatis adipisci consectetur
+                dignissimos vitae, cumque rerum! Similique placeat impedit ipsum
+                reiciendis unde veritatis magni blanditiis fugit hic
+                voluptatibus id voluptatem ipsa, tenetur maiores. Consequatur
+                et, eos debitis, commodi nihil quaerat cumque magni suscipit
+                ullam quibusdam corporis ex similique doloribus laudantium,
+                voluptates praesentium possimus illum quam. Ipsum itaque
+                excepturi id dolor corporis! At distinctio magnam ab eveniet
+                neque natus sint molestiae perspiciatis. Voluptatem odit
+                praesentium eum nesciunt vero amet ex error? Officia reiciendis
+              </CustomText>
+            </View>
+            <View className="bg-[#FFBF23] h-[30%] w-full mt-auto rounded-b-xl justify-between p-1.5">
+              <CustomText fontThicknessNumber={3} className="text-base">
+                The Last Trip
+              </CustomText>
+              <View className="flex flex-row justify-between w-full">
+                <CustomText>07/02/2023</CustomText>
+                <CustomText>8:00 PM</CustomText>
+              </View>
+            </View>
           </View>
         )}
       />
