d4094d94b7d2f81ea3c9f1622b9dca71902da145
importTahsinZaman <tahsinz21366@gmail.com>
2023-07-02 05:04:00 -0400

    Added scan editing

--- app/screens/TextSelectScreen.tsx
+++ app/screens/TextSelectScreen.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 
 import {
+  View,
   TouchableOpacity,
   SafeAreaView,
-  ScrollView,
   Dimensions,
   Platform,
   StatusBar,
+  Button,
+  Modal,
+  TextInput,
+  KeyboardAvoidingView,
 } from "react-native";
 
 import { OCR_SPACE_API_KEY } from "@env";
@@ -17,6 +21,7 @@ import CustomText from "../components/CustomText";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import SkeletonComponent from "../components/SkeletonComponent";
+import { FlatList } from "react-native-gesture-handler";
 
 const PAGE_WIDTH = Dimensions.get("window").width;
 
@@ -24,7 +29,16 @@ export default function TextSelectScreen({ route, navigation }) {
   const { ReturnHome, BackNavigation } = route.params;
   const [gettingText, setGettingText] = useState(false);
   const [ocrText, setOcrText] = useState("");
-  const [formattedText, setFormattedText] = useState(null);
+  const [formattedText, setFormattedText] = useState<
+    | {
+        sentence: string;
+        index: number;
+      }[]
+    | null
+  >(null);
+  const [selectedTextIndex, setSelectedTextIndex] = useState(0);
+  const [editingText, setEditingText] = useState(false);
+  const [editedText, setEditedText] = useState("");
 
   const isFocused = useIsFocused();
 
@@ -38,6 +52,7 @@ export default function TextSelectScreen({ route, navigation }) {
         getOcrText(base64);
       }
     } else {
+      setSelectedTextIndex(0);
       setFormattedText(null);
     }
   }, [isFocused]);
@@ -61,6 +76,87 @@ export default function TextSelectScreen({ route, navigation }) {
     } catch (e) {
       console.log("GET SAVED OCR TEXT ERROR:" + e);
     }
+  };
+
+  const updateRecentScanStorage = async (newMostRecentScan: string) => {
+    let oldSavedScans = [];
+    let newSavedScans = [];
+
+    oldSavedScans = await AsyncStorage.multiGet([
+      "RecentScan0",
+      "RecentScan1",
+      "RecentScan2",
+      "RecentScan3",
+      "RecentScan4",
+    ]);
+
+    newSavedScans = [
+      ["RecentScan0", newMostRecentScan],
+      ["RecentScan1", oldSavedScans[0][1] == null ? "" : oldSavedScans[0][1]],
+      ["RecentScan2", oldSavedScans[1][1] == null ? "" : oldSavedScans[1][1]],
+      ["RecentScan3", oldSavedScans[2][1] == null ? "" : oldSavedScans[2][1]],
+      ["RecentScan4", oldSavedScans[3][1] == null ? "" : oldSavedScans[3][1]],
+    ];
+
+    await AsyncStorage.multiSet([
+      newSavedScans[0],
+      newSavedScans[1],
+      newSavedScans[2],
+      newSavedScans[3],
+      newSavedScans[4],
+    ]);
+
+    ///////////////////////////////////
+
+    const d = new Date();
+    const date = d.toLocaleDateString();
+    let time = d.toLocaleTimeString();
+    time = time.slice(0, 4) + time.slice(7);
+
+    let oldSavedScanTimes = [];
+    let newSavedScanTimes = [];
+
+    oldSavedScanTimes = await AsyncStorage.multiGet([
+      "RecentScanTime0",
+      "RecentScanTime1",
+      "RecentScanTime2",
+      "RecentScanTime3",
+      "RecentScanTime4",
+    ]);
+
+    newSavedScanTimes = [
+      ["RecentScanTime0", date + "&$&" + time],
+      [
+        "RecentScanTime1",
+        oldSavedScanTimes[0][1] == null ? "" : oldSavedScanTimes[0][1],
+      ],
+      [
+        "RecentScanTime2",
+        oldSavedScanTimes[1][1] == null ? "" : oldSavedScanTimes[1][1],
+      ],
+      [
+        "RecentScanTime3",
+        oldSavedScanTimes[2][1] == null ? "" : oldSavedScanTimes[2][1],
+      ],
+      [
+        "RecentScanTime4",
+        oldSavedScanTimes[3][1] == null ? "" : oldSavedScanTimes[3][1],
+      ],
+    ];
+
+    await AsyncStorage.multiSet([
+      newSavedScanTimes[0],
+      newSavedScanTimes[1],
+      newSavedScanTimes[2],
+      newSavedScanTimes[3],
+      newSavedScanTimes[4],
+    ]);
+  };
+
+  const replaceMostRecentScanStorage = async (
+    newMostRecentScanText: string
+  ) => {
+    await AsyncStorage.setItem("RecentScan0", newMostRecentScanText);
   };
 
   const getOcrText = (base64) => {
@@ -95,90 +191,7 @@ export default function TextSelectScreen({ route, navigation }) {
 
         if (result.ParsedResults[0].ParsedText) {
           try {
-            let oldSavedScans = [];
-            let newSavedScans = [];
-
-            oldSavedScans = await AsyncStorage.multiGet([
-              "RecentScan0",
-              "RecentScan1",
-              "RecentScan2",
-              "RecentScan3",
-              "RecentScan4",
-            ]);
-
-            newSavedScans = [
-              ["RecentScan0", result.ParsedResults[0].ParsedText],
-              [
-                "RecentScan1",
-                oldSavedScans[0][1] == null ? "" : oldSavedScans[0][1],
-              ],
-              [
-                "RecentScan2",
-                oldSavedScans[1][1] == null ? "" : oldSavedScans[1][1],
-              ],
-              [
-                "RecentScan3",
-                oldSavedScans[2][1] == null ? "" : oldSavedScans[2][1],
-              ],
-              [
-                "RecentScan4",
-                oldSavedScans[3][1] == null ? "" : oldSavedScans[3][1],
-              ],
-            ];
-
-            await AsyncStorage.multiSet([
-              newSavedScans[0],
-              newSavedScans[1],
-              newSavedScans[2],
-              newSavedScans[3],
-              newSavedScans[4],
-            ]);
-
-            ///////////////////////////////////
-
-            const d = new Date();
-            const date = d.toLocaleDateString();
-            let time = d.toLocaleTimeString();
-            time = time.slice(0, 4) + time.slice(7);
-
-            let oldSavedScanTimes = [];
-            let newSavedScanTimes = [];
-
-            oldSavedScanTimes = await AsyncStorage.multiGet([
-              "RecentScanTime0",
-              "RecentScanTime1",
-              "RecentScanTime2",
-              "RecentScanTime3",
-              "RecentScanTime4",
-            ]);
-
-            newSavedScanTimes = [
-              ["RecentScanTime0", date + "&$&" + time],
-              [
-                "RecentScanTime1",
-                oldSavedScanTimes[0][1] == null ? "" : oldSavedScanTimes[0][1],
-              ],
-              [
-                "RecentScanTime2",
-                oldSavedScanTimes[1][1] == null ? "" : oldSavedScanTimes[1][1],
-              ],
-              [
-                "RecentScanTime3",
-                oldSavedScanTimes[2][1] == null ? "" : oldSavedScanTimes[2][1],
-              ],
-              [
-                "RecentScanTime4",
-                oldSavedScanTimes[3][1] == null ? "" : oldSavedScanTimes[3][1],
-              ],
-            ];
-
-            await AsyncStorage.multiSet([
-              newSavedScanTimes[0],
-              newSavedScanTimes[1],
-              newSavedScanTimes[2],
-              newSavedScanTimes[3],
-              newSavedScanTimes[4],
-            ]);
+            updateRecentScanStorage(result.ParsedResults[0].ParsedText);
           } catch (e) {
             console.log("Setting new saved scans error: ", e);
           }
@@ -193,8 +206,41 @@ export default function TextSelectScreen({ route, navigation }) {
     let textArray = preText
       .replace(/(\.+|\:|\!|\?)(\"*|\'*|\)*|}*|]*)(\s|\n|\r|\r\n)/gm, "$1$2|")
       .split("|");
-    setFormattedText(textArray);
+
+    function createArrayOfSentences(strings: string | any[]) {
+      var result = [];
+
+      for (var i = 0; i < strings.length; i++) {
+        var obj = {
+          sentence: strings[i],
+          index: i,
+        };
+
+        result.push(obj);
+      }
+
+      return result;
+    }
+
+    setFormattedText(createArrayOfSentences(textArray));
   };
+
+  type ItemProps = { sentence: string; onPress: () => void; selected: boolean };
+
+  const SentenceItem = ({ sentence, onPress, selected }: ItemProps) => (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        className="my-1 p-2 rounded-lg"
+        style={{
+          borderColor: selected ? "#FFBF23" : "white",
+          borderWidth: selected ? 2 : 0,
+          backgroundColor: selected ? "#FFBF2329" : "white",
+        }}
+      >
+        <CustomText className="text-lg text-[#8D8D8D]">{sentence}</CustomText>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <SafeAreaView
@@ -210,34 +256,119 @@ export default function TextSelectScreen({ route, navigation }) {
         destination={ReturnHome ? "Home" : "Scan"}
         showNavBar={ReturnHome ? true : false}
       ></ReturnHeader>
-      <ScrollView className=" mx-4 ">
-        {formattedText && !gettingText ? (
-          formattedText.map((sentence, index) => {
-            return (
-              <TouchableOpacity
-                key={index}
-                onPress={() =>
-                  navigation.navigate("Translation", {
-                    sentence: sentence,
-                    ReturnHome: ReturnHome,
-                  })
-                }
-                className=" p-2 my-1 rounded-lg"
-              >
-                <CustomText className="text-lg text-[#8D8D8D]">
-                  {sentence}
-                </CustomText>
-              </TouchableOpacity>
-            );
-          })
-        ) : (
+      {formattedText && !gettingText ? (
+        // formattedText.map((sentence, index) => {
+        //   return (
+        //     <TouchableOpacity
+        //       key={index}
+        //       onPress={() =>
+        //         navigation.navigate("Translation", {
+        //           sentence: sentence,
+        //           ReturnHome: ReturnHome,
+        //         })
+        //       }
+        //       className=" p-2 my-1 rounded-lg"
+        //     >
+        //       <CustomText className="text-lg text-[#8D8D8D]">
+        //         {sentence}
+        //       </CustomText>
+        //     </TouchableOpacity>
+        //   );
+        // })
+        <FlatList
+          className="mx-4"
+          data={formattedText}
+          renderItem={({ item }) => (
+            <SentenceItem
+              sentence={item.sentence}
+              onPress={() => setSelectedTextIndex(item.index)}
+              selected={item.index === selectedTextIndex}
+            />
+          )}
+          keyExtractor={(item) => item.index.toString()}
+          extraData={[selectedTextIndex, formattedText]}
+        ></FlatList>
+      ) : (
+        <View className="mx-4">
           <SkeletonComponent
             marginHorizontal={2}
             count={15}
             width={PAGE_WIDTH * 0.91545893719}
           />
-        )}
-      </ScrollView>
+        </View>
+      )}
+
+      {formattedText && !gettingText && (
+        <View className="flex flex-row w-full justify-around">
+          <Button
+            title="Edit"
+            color="#FFBF23"
+            onPress={() => {
+              if (formattedText[selectedTextIndex].sentence) {
+                setEditingText(true);
+              }
+            }}
+          />
+          <Button
+            title="Next"
+            color="#FFBF23"
+            onPress={() => {
+              if (formattedText[selectedTextIndex].sentence) {
+                navigation.navigate("Translation", {
+                  sentence: formattedText[selectedTextIndex].sentence,
+                  ReturnHome: ReturnHome,
+                });
+              }
+            }}
+          />
+        </View>
+      )}
+
+      {editingText && (
+        <Modal
+          visible={editingText}
+          transparent={false}
+          onRequestClose={() => setEditingText(false)}
+          animationType="slide"
+        >
+          <KeyboardAvoidingView
+            className="flex justify-center items-center h-full w-full"
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <View
+              className="bg-[#F6F6F6] rounded-xl p-4 my-4 min-h-[31vh] max-h-[31vh] min-w-[92vw]"
+              style={{ marginHorizontal: PAGE_WIDTH * 0.05333333333 }}
+            >
+              <TextInput
+                style={{ fontSize: 16 }}
+                editable
+                multiline
+                defaultValue={formattedText[selectedTextIndex].sentence}
+                maxLength={200}
+                onChangeText={(newText) => setEditedText(newText)}
+              ></TextInput>
+            </View>
+            <Button
+              title="Done"
+              color="#FFBF23"
+              onPress={() => {
+                let newFormattedText = formattedText;
+                newFormattedText[selectedTextIndex].sentence = editedText;
+
+                let fullText = "";
+                for (let i = 0; i < newFormattedText.length; i++) {
+                  const sentence = newFormattedText[i].sentence;
+                  fullText += sentence + " ";
+                }
+
+                replaceMostRecentScanStorage(fullText);
+                setFormattedText(newFormattedText);
+                setEditingText(false);
+              }}
+            ></Button>
+          </KeyboardAvoidingView>
+        </Modal>
+      )}
     </SafeAreaView>
   );
 }
